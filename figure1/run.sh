2f9e9165b90437fdf90861f49a17bcf936b1fd66
Michael Sevilla <mikesevilla3@gmail.com>
2016-04-09 10:08:25 -0600

    experiment (scalability): partial results
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- figure1/run.sh
+++ figure1/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -r results/* logs/* >> /dev/null 2>&1
 set -e
 set -x
 
 # run all tmpfs tests
-for w in dd samtools git ceph kernel; do
+#for w in dd git ceph kernel; do
+for w in ceph kernel; do
     export ANSIBLE_LOG_PATH="logs/ansible-tmpfs-${w}.log"
     export INVENTORY="inventory/1-node"
     export ARGS="-e @vars/all.yml -e @vars/tmpfs.yml -e nnodes=1 -i $INVENTORY"
@@ -14,7 +14,7 @@ for w in dd samtools git ceph kernel; do
 done
 
 # run scalability tests
-for w in dd samtools git ceph kernel; do
+for w in dd git ceph kernel; do
   for i in 1 2 3 4 5 6; do
     for s in udp; do
       export ANSIBLE_LOG_PATH="logs/ansible-${s}-${w}.log"
@@ -25,3 +25,4 @@ for w in dd samtools git ceph kernel; do
     done
   done
 done
+
